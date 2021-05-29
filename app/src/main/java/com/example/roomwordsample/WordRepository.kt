9db78bc51ccfbdef5c7e94e71db007544c6e49e0
kunakbar <akbarpamungkas08@gmail.com>
2021-05-29 22:20:54 +0700

    first commit

---
+++ app/src/main/java/com/example/roomwordsample/WordRepository.kt
@@ -0,0 +1,17 @@
+package com.example.roomwordsample
+
+import androidx.annotation.WorkerThread
+import kotlinx.coroutines.flow.Flow
+
+
+class WordRepository(private val wordDao: WordDao) {
+
+    val allWords: List<Word> = wordDao.getAlphabetizedWords()
+
+    @Suppress("RedundantSuspendModifier")
+    @WorkerThread
+    suspend fun insert(word: Word) {
+        wordDao.insert(word)
+    }
+}
+
